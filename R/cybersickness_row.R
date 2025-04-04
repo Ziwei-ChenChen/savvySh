a112aca73caf41818362b974e81335fef95e439f
Ziwei Chen <163039518+Ziwei-ChenChen@users.noreply.github.com>
2025-04-04 11:34:17 +0100

    Initial commit

--- R/cybersickness_row.R
+++ R/cybersickness_row.R
@@ -1,18 +1,19 @@
 #' Raw Physiological Measurements for Cybersickness Study
 #'
-#' @description Raw data from a cybersickness study including heart rate, heart rate variability, and their percent changes.
+#'Raw data from a cybersickness study including heart rate, heart rate variability, and their percent changes.
 #'
-#' @format A data frame with 25893 rows and 17 columns:
-#' \describe{
-#'   \item{Epoch}{Time interval index.}
-#'   \item{HR}{Heart Rate.}
-#'   \item{PC_HR}{Percent change in Heart Rate.}
-#'   \item{HR_MIN}{Minimum heart rate in the epoch.}
-#'   \item{HR_MAX}{Maximum heart rate in the epoch.}
-#'   \item{HRV}{Heart Rate Variability.}
-#'   \item{PC_HRV}{Percent change in HRV.}
-#'   \item{HRV_MIN}{Minimum HRV in the epoch.}
-#'   \item{HRV_MAX}{Maximum HRV in the epoch.}
-#'   \item{...}{Other columns include similar physiological signals.}
-#' }
+#'A data frame with 25893 rows and 17 columns:
+#'Epoch: Time interval index.
+#'HR: Heart Rate.
+#'PC_HR: Percent change in Heart Rate.
+#'HR_MIN: Minimum heart rate in the epoch.
+#'HR_MAX: Maximum heart rate in the epoch.
+#'HRV: Heart Rate Variability.
+#'PC_HRV: Percent change in HRV.
+#'HRV_MIN: Minimum HRV in the epoch.
+#'HRV_MAX: Maximum HRV in the epoch.
+#'...: Other columns include similar physiological signals.
+#'
 "cybersickness_row"
+
+
--- R/returns_441.R
+++ R/returns_441.R
@@ -4,10 +4,9 @@
 #' adjusted for dividends, over 6037 time periods. Each column corresponds to a unique stock,
 #' and the first column is the date of observation.
 #'
-#' @format A data frame with 6037 rows and 442 columns:
-#' \describe{
-#'   \item{Date}{Date of the return observation (format: YYYY-MM-DD).}
-#'   \item{PERMNO_*}{Each remaining column corresponds to one stock's return.}
-#' }
+#'A data frame with 6037 rows and 442 columns:
+#'
+#'Date: Date of the return observation (format: YYYY-MM-DD).
+#'PERMNO_*: Each remaining column corresponds to one stock's return.
 #'
 "returns_441"
