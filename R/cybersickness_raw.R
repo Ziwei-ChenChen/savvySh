c5cddff67ef4593f8f33eed4fec22a2b4bdc1ea2
Ziwei Chen <163039518+Ziwei-ChenChen@users.noreply.github.com>
2025-04-04 17:28:36 +0100

    Initial commit

---
+++ R/cybersickness_raw.R
@@ -0,0 +1,18 @@
+#' Raw Physiological Measurements for Cybersickness Study
+#'
+#' Raw data from a cybersickness study including heart rate, heart rate variability, and their percent changes.
+#'
+#' A data frame with 25893 rows and 17 columns:
+#' Epoch: Time interval index.
+#' HR: Heart Rate.
+#' PC_HR: Percent change in Heart Rate.
+#' HR_MIN: Minimum heart rate in the epoch.
+#' HR_MAX: Maximum heart rate in the epoch.
+#' HRV: Heart Rate Variability.
+#' PC_HRV: Percent change in HRV.
+#' HRV_MIN: Minimum HRV in the epoch.
+#' HRV_MAX: Maximum HRV in the epoch.
+#' ...: Other columns include similar physiological signals.
+#'
+#' Note: This data is also available on https://github.com/shovonis/CyberSicknessClassification/tree/master/data_preprocessing/All%20raw%20data
+"cybersickness_raw"
